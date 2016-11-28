f0a3bb6bab7dcc51adec585b2d44400d31accdce
Mark <twcloudwebapp@outlook.com>
2016-11-28 11:47:12 +0800

    fix 2 bugs, allow no data on 2 fields, list show mold num

--- mysite/app001/models.py
+++ mysite/app001/models.py
@@ -37,14 +37,14 @@ class Flowchart(models.Model):
 class Flowchartprocess(models.Model):
     # num = models.IntegerField(default=0,verbose_name="第幾式")
     flowchart = models.ForeignKey(Flowchart, on_delete=models.CASCADE)
-    a = models.IntegerField(blank=True, null=True, verbose_name="序號")
+    a = models.IntegerField(blank=True, null=True,verbose_name="序號")
     b = models.CharField(default = '.', max_length=32,verbose_name="部門")
     c = models.CharField(max_length=32,verbose_name="工序")
     d = models.CharField(max_length=128,verbose_name="過程描述")
     e = models.CharField(default= ".", max_length=256,verbose_name="品質要求")
-    f = models.CharField(default= ".", max_length=32,verbose_name="設備")
-    g = models.CharField(default= ".", max_length=32,verbose_name="刀、治、模具")
-    h = models.CharField(default= ".", max_length=32,verbose_name="檢具")
+    f = models.CharField(blank=True, null=True,  max_length=32,verbose_name="設備")
+    g = models.CharField(blank=True, null=True,  max_length=32,verbose_name="刀、治、模具")
+    h = models.CharField(blank=True, null=True,  max_length=32,verbose_name="檢具")
 
 
     # remarks = models.CharField(max_length=200)
