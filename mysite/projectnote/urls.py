175299ca6470bbab086b10c739a4d79192728799
Mark <twcloudwebapp@outlook.com>
2016-11-30 16:58:40 +0800

    to check fc list

--- mysite/projectnote/urls.py
+++ mysite/projectnote/urls.py
@@ -18,6 +18,15 @@ urlpatterns = [
     url(r'^flowchart$', views.flowchart, name='flowchart'),
     url(r'^flowchart_list$', views.flowchart_list, name='flowchart_list'),
 
+    # url(r'^fc/$', views.view_flowchart_list, name='view_flowchart_list'),
+    # url(r'^fc2/$', views.Flowchartprocess2, name='Flowchartprocess2'),
+    # url(r'^fc3/$', views.Flowchartprocess3, name='Flowchartprocess3'),
+    # url(r'^p1/$', views.p1, name='p1'),
+    # url(r'^fc/(?P<item_id>[_A-Za-z0-9-\#\\+]+)', views.view_flowchart, name='view_flowchart'), #item001/123 後面有東西都好
+
+
+
+
     # url(r'^test1$', DefaultFormsetView.as_view(), name='formset_default'),
     url(r'^formset$', DefaultFormsetView.as_view(), name='formset_default'),
     url(r'^form$', DefaultFormView.as_view(), name='form_default'),
