e216488cc019f1b7c8cd0cbc3db7d7d078be0b01
Mark <twcloudwebapp@outlook.com>
2016-11-30 16:16:38 +0800

    show flowchart on projectnote admin

--- mysite/projectnote/admin.py
+++ mysite/projectnote/admin.py
@@ -15,3 +15,40 @@ from .models import  Note
 class NoteAdmin(ImportExportModelAdmin):
     list_display=['date1','tag','where','note','date2','fix']
 admin.site.register(Note,NoteAdmin)
+
+from .models import Flowchart, Flowchartprocess
+
+class ProcessInline(admin.TabularInline):
+    model = Flowchartprocess
+    extra = 3
+
+
+# class FlowchartAdmin(admin.ModelAdmin):
+#     list_display = ('part_name','cust_name','by1')
+#     fieldsets = [
+#         (None,               {'fields': ['part_name','cust_name',]}),
+#         ('基礎信息', {'fields': ['c1','c2','e1','e2','g1','g2',], 'classes': ['collapse']}),
+#
+#         ('人員日期', {'fields': ['by1','by1date','by2','by2date','by3','by3date',], 'classes': ['collapse']}),
+#         # ('人員日期', {'fields': ['by1','by1date','by2','by2date','by2','by2date',], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ProcessInline]
+#
+# admin.site.register(Flowchart, FlowchartAdmin)
+
+class FlowchartAdmin(ImportExportModelAdmin):
+    list_display = ('part_name','cust_name','by1')
+    fieldsets = [
+        (None,               {'fields': ['part_name','cust_name',]}),
+        ('基礎信息', {'fields': ['c1','c2','e1','e2','g1','g2',], 'classes': ['collapse']}),
+
+        ('人員日期', {'fields': ['by1','by1date','by2','by2date','by3','by3date',], 'classes': ['collapse']}),
+        # ('人員日期', {'fields': ['by1','by1date','by2','by2date','by2','by2date',], 'classes': ['collapse']}),
+    ]
+    inlines = [ProcessInline]
+admin.site.register(Flowchart,FlowchartAdmin)
+
+
+class FlowchartprocessAdmin(ImportExportModelAdmin):
+    list_display=['flowchart','a','b','c','d','e','f','g','h']
+admin.site.register(Flowchartprocess,FlowchartprocessAdmin)
