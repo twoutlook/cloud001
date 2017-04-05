4d63099cec1424f0235517f02bf3d3d36d03f61d
Mark <twcloudwebapp@outlook.com>
2017-04-05 14:56:44 +0800

    array array testing

---
+++ mysite/projectnote/templates/projectnote/js.js
@@ -0,0 +1,30 @@
+$js_a1="12345";
+$js_a2="ABC";
+
+lbl_array=[];
+lbl_array.push(""); //1
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");//7
+lbl_array.push("");//8
+lbl_array.push("");//9
+lbl_array.push("");//10
+lbl_array.push(""); //1
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");
+lbl_array.push("");//7
+lbl_array.push("");//8
+lbl_array.push(new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME"););//19
+lbl_array.push("");//10
+
+
+// lbl_array.push(new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME"););//19
+
+
+
