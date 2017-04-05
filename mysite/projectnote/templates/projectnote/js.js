0841a49e12812bb6f5fdd66ab1ef8f37e7ca3521
Mark <twcloudwebapp@outlook.com>
2017-04-05 15:12:27 +0800

    edit js.js, wrong assignment

--- mysite/projectnote/templates/projectnote/js.js
+++ mysite/projectnote/templates/projectnote/js.js
@@ -4,9 +4,12 @@ $js_a2="ABC";
 lbl= new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME");
 
 lbl_array= new Array(); 
-lbl_array[19]=lbl_array;
+lbl_array[19]=lbl;
 
-
+ // lbl = new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME");
+  
+ // var sql = " select a.DEPT_ID,a.DEPT_NAME,a.EMPE_ID,a.EMPE_NAME,a.JOB,a.LEAVE_DATE,a.SUP_NAME from V_EMPE a  ";
+ 
 // lbl_array.push(new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME"););//19
 
 
