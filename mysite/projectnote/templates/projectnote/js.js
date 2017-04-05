7a167cd665f2f09a25632e5e09dded15da049055
Mark <twcloudwebapp@outlook.com>
2017-04-05 15:07:19 +0800

    fix js.js

--- mysite/projectnote/templates/projectnote/js.js
+++ mysite/projectnote/templates/projectnote/js.js
@@ -1,27 +1,10 @@
 $js_a1="12345";
 $js_a2="ABC";
 
-lbl_array=[];
-lbl_array.push(""); //1
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");//7
-lbl_array.push("");//8
-lbl_array.push("");//9
-lbl_array.push("");//10
-lbl_array.push(""); //1
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");
-lbl_array.push("");//7
-lbl_array.push("");//8
-lbl_array.push(new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME"););//19
-lbl_array.push("");//10
+lbl=new new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME");
+
+lbl_array=new new Array(); 
+lbl_array[19]=lbl_array;
 
 
 // lbl_array.push(new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME"););//19
