95d2802651ae6df51268b276570e16b4648afb52
Mark <twcloudwebapp@outlook.com>
2017-04-05 15:09:45 +0800

    fix js.js, double new

--- mysite/projectnote/templates/projectnote/js.js
+++ mysite/projectnote/templates/projectnote/js.js
@@ -1,9 +1,9 @@
 $js_a1="12345";
 $js_a2="ABC";
 
-lbl=new new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME");
+lbl= new Array("DEPT_ID", "DEPT_NAME", "EMPE_ID","EMPE_NAME","JOB","LEAVE_DATE","SUP_NAME");
 
-lbl_array=new new Array(); 
+lbl_array= new Array(); 
 lbl_array[19]=lbl_array;
 
 
