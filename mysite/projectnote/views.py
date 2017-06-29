5f1ad6263608fa4b1e0ee849c50f90541deaefc9
Mark <twcloudwebapp@outlook.com>
2017-06-29 10:30:59 +0800

    fix after login redirect

--- mysite/projectnote/views.py
+++ mysite/projectnote/views.py
@@ -550,7 +550,7 @@ def t100_report_list2(request):
 def t100_dept_report_list(request, dept_id):
     is_grpxxx=request.user.groups.filter(name='grp005').exists()
     if not is_grpxxx:
-       return redirect('/admin/login/?next=/projectnote/t100_report_list2')
+       return redirect('/admin/login/?next=/projectnote/t100_dept_report')
     current_user=request.user.username
     # item_list = Sop.objects.filter(is_active = True).order_by('code')[:500]
     # TO SHOW ALL , INCLUDING ACTIVE OR NOT
